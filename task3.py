0fe7eaa645d56f2f4feab3982cdd15571fdee45c
krichard07 <152333892+krichard07@users.noreply.github.com>
2023-11-29 21:05:04 +0100

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,6 @@
+num1 = int(input("Please enter the first number: "))
+num2 = int(input("Please enter the second number: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
